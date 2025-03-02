6b7f195cd3ce9240eec80bda9c19172f3d40cfb4
KingMG <kingmg320@gmail.com>
2025-03-02 03:38:33 +0330

    enhance ShopCartPage; display message when cart is empty and prompt to continue shopping

--- frontend/src/pages/ShopCartPage.tsx
+++ frontend/src/pages/ShopCartPage.tsx
@@ -5,17 +5,32 @@ import { useCartStore } from "../store/useCartStore";
 
 const ShopCartPage = () => {
   const { Cart } = useCartStore();
+
   return (
     <div className="min-h-[calc(100vh-24.2rem)]   px-5 ">
       <div className=" flex flex-col items-center justify-center">
         <div className="max-w-5xl h-full w-full ">
           <Breadcrumbs newDirectory="Cart" />
-          <div className=" w-full flex justify-between  flex-col lg:flex-row ">
-            {Cart.length > 0 && <CartTable />}
-            <div className="flex justify-end">
-              <CheckOut />
+          {Cart.length > 0 ? (
+            <div className=" w-full flex justify-between  flex-col lg:flex-row ">
+              <CartTable />
+              <div className="flex justify-end">
+                <CheckOut />
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center py-10">
+              <h2 className="text-2xl font-semibold mb-4">
+                Your cart is currently empty.
+              </h2>
+              <p className="text-center mb-6">
+                Browse our categories and discover our best deals!
+              </p>
+              <a href="/" className="text-blue-500 hover:underline">
+                Continue Shopping
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </div>
