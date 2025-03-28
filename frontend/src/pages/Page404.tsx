234f8800c4cfaeca855cf46b8513a40fe6088103
KingMG <kingmg320@gmail.com>
2025-03-28 06:13:38 +0330

    enhance 404 page layout with improved styling and image display

--- frontend/src/pages/Page404.tsx
+++ frontend/src/pages/Page404.tsx
@@ -1,12 +1,21 @@
+import page404 from "../assets/404Page.svg";
+
 const Page404 = () => {
   return (
     <div className="min-h-[calc(100vh-24.2rem)]">
-      <div className="flex flex-col items-center  justify-center max-w-5xl h-full mx-auto ">
-        <h1 className="text-6xl font-bold m-4">404</h1>
-        <p className="text-xl mb-8">Page Not Found</p>
-        <a href="/" className="text-blue-500 hover:underline">
-          Go back to Home
-        </a>
+      <div className="flex flex-col-reverse md:flex-row gap-5  items-center  justify-around max-w-5xl h-full mx-auto px-8 ">
+        <div className="text-center md:text-left">
+          <h1 className="text-6xl font-bold mb-4">404</h1>
+          <p className="text-xl mb-6 text-gray-600">
+            Sorry, the page you are looking for does not exist.
+          </p>
+          <a href="/" className="btn btn-neutral btn-wide">
+            Go Back to Home
+          </a>
+        </div>
+        <div className="max-w-xl w-full">
+          <img src={page404} alt="404 page" />
+        </div>
       </div>
     </div>
   );
